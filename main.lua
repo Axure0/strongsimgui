1e5aea132abee77fb33d559a1b40012edcf28523
Axure <96837058+Axure0@users.noreply.github.com>
2024-11-11 21:03:02 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-loadstring("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/MainGUI.lua")
-loadstring("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/minGUI.lua")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/MainGUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/minGUI.lua"))()
