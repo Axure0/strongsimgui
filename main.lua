2ef0aa0d0f02ba77e713969698e443214294083f
Axure <96837058+Axure0@users.noreply.github.com>
2024-11-11 20:59:29 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
 loadstring("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/MainGUI.lua")
+loadstring("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/minGUI.lua")
