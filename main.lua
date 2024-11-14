7ae99d204258518b0f400724ff4d13a6733ee05f
Axure <96837058+Axure0@users.noreply.github.com>
2024-11-14 20:05:07 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/MainGUI.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/minGUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/maingui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/mingui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Axure0/strongsimgui/refs/heads/main/func.lua"))()
